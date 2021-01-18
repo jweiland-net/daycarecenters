60b178a0e9571e2407099ade06b576d464bcd56c
Stefan Froemken <froemken@gmail.com>
2021-01-18 15:15:26 +0100

    Convert time VH to renderStatic

--- Classes/ViewHelpers/TimeViewHelper.php
+++ Classes/ViewHelpers/TimeViewHelper.php
@@ -13,21 +13,23 @@ namespace JWeiland\Daycarecenters\ViewHelpers;
 
 use JWeiland\Daycarecenters\Converter\TimeToStringConverter;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
- * This ViewHelper converts seconds since midnight 0:00 to a readable format
+ * This ViewHelper converts seconds since midnight to a readable format 0:00
  */
 class TimeViewHelper extends AbstractViewHelper
 {
-    /**
-     * This ViewHelper converts seconds since midnight 0:00 to a readable format
-     *
-     * @return string
-     */
-    public function render(): string
-    {
-        $timestamp = (int)$this->renderChildren();
+    use CompileWithRenderStatic;
+
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $timestamp = (int)$renderChildrenClosure();
         $converter = GeneralUtility::makeInstance(TimeToStringConverter::class);
 
         return $converter->convert($timestamp);
