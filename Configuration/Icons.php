9d8d046a2896da9ba5a09a56255a70013e2d253e
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-06-10 10:07:25 +0200

    [BUGFIX] Removed local variables inside Icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
-// Register SVG Icon Identifier
-$svgIcons = [
-    'ext-daycarecenters-careform' => 'careform.svg',
-    'ext-daycarecenters-district' => 'district.svg',
-    'ext-daycarecenters-holder' => 'holder.svg',
-    'ext-daycarecenters-kita' => 'kita.svg',
-    'ext-daycarecenters-telephone' => 'telephone.svg',
-];
-
-$configuredIcons = [];
-foreach ($svgIcons as $identifier => $fileName) {
-    $icons[$identifier] = [
+return [
+    'ext-daycarecenters-careform' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:daycarecenters/Resources/Public/Icons/' . $fileName
-    ];
-}
-
-return $configuredIcons;
+        'source' => 'EXT:daycarecenters/Resources/Public/Icons/careform.svg',
+    ],
+    'ext-daycarecenters-district' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:daycarecenters/Resources/Public/Icons/district.svg',
+    ],
+    'ext-daycarecenters-holder' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:daycarecenters/Resources/Public/Icons/holder.svg',
+    ],
+    'ext-daycarecenters-kita' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:daycarecenters/Resources/Public/Icons/kita.svg',
+    ],
+    'ext-daycarecenters-telephone' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:daycarecenters/Resources/Public/Icons/telephone.svg',
+    ],
+];
