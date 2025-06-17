d00bf6006769ac4b9841205f446f3e60443560cc
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-06-17 17:27:08 +0200

    [TASK] Fixed ViewHelpers replacing the deprecated renderStatic methods

--- Classes/ViewHelpers/TimeViewHelper.php
+++ Classes/ViewHelpers/TimeViewHelper.php
@@ -15,21 +15,15 @@ use JWeiland\Daycarecenters\Converter\TimeToStringConverter;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * This ViewHelper converts seconds since midnight to a readable format 0:00
  */
 class TimeViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
-    public static function renderStatic(
-        array $arguments,
-        \Closure $renderChildrenClosure,
-        RenderingContextInterface $renderingContext
-    ) {
-        return self::getTimeToStringConverter()->convert((int)$renderChildrenClosure());
+    public function render()
+    {
+        return self::getTimeToStringConverter()->convert((int)$this->renderChildrenClosure());
     }
 
     private static function getTimeToStringConverter(): TimeToStringConverter
