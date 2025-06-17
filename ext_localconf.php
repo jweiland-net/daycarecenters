dee3ee9bded997b15bfa1f48d6b3e36277ab0610
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-06-17 04:28:36 +0200

    [TASK] Fixed FQCN with namespace imports

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,29 @@
 <?php
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use JWeiland\Daycarecenters\Controller\KitaController;
+use JWeiland\Daycarecenters\Controller\HolderController;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
 call_user_func(static function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'Daycarecenters',
         'Daycarecenters',
         [
-            \JWeiland\Daycarecenters\Controller\KitaController::class => 'list, show, search',
-            \JWeiland\Daycarecenters\Controller\HolderController::class => 'show',
+            KitaController::class => 'list, show, search',
+            HolderController::class => 'show',
         ],
         // non-cacheable actions
         [
-            \JWeiland\Daycarecenters\Controller\KitaController::class => 'search',
+            KitaController::class => 'search',
         ]
     );
 
     // Add daycarecenters plugin to new element wizard
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:daycarecenters/Configuration/TSconfig/ContentElementWizard.tsconfig">'
     );
 });
